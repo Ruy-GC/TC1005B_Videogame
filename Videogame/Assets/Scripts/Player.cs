ac28592775971f287fc8c7952ada7da77a580cf8
Ruy Guzmán Camacho <ruy.guzman2002@gmail.com>
2022-03-01 22:12:00 -0600

    initial movement and ground check

---
+++ Videogame/Assets/Scripts/Player.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //movement
+    public float velocidad;
+    public Rigidbody2D rb;
+    public float jumpAmount = 10;
+
+    //ground check
+    public float distanceToCheck = 0.1f;
+    bool isGrounded;
+
+    public float jumpForce = 20;
+    public float gravity = -9.81f;
+    public float gravityScale = 5;
+    float velocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        //horizontal movement
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Translate(
+            velocidad * horizontal * Time.deltaTime,
+            0,
+            0,
+            Space.World
+            );
+
+         
+        //checks if character is grounded using raycast
+        //we set the origin of the raycast at the bottom of the character
+        if (Physics2D.Raycast(transform.position + new Vector3(0f,-1f,0f), Vector3.down, distanceToCheck))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        //jumping
+        //on pressing space or joystick button 0 (A(xbox), X(ps), B (switch)
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown("joystick button 0"))
+        {
+
+            if (isGrounded){
+                //adds an impulse in the up direction of a 2D vector
+                rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+            }
+
+        }
+    
+    }
+}
