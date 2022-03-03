949007c6a7cf315de9c8addfd11fe1c95f590fdd
Ruy Guzmán Camacho <ruy.guzman2002@gmail.com>
2022-03-02 21:17:18 -0600

    Finished initial movement and platform interaction

--- Assets/Scripts/JumpOffPlatform.cs
+++ Assets/Scripts/JumpOffPlatform.cs
@@ -18,22 +18,25 @@ public class JumpOffPlatform : MonoBehaviour
     {
 
         //Checks if the down arrow has been released
-        if(Input.GetKeyUp(KeyCode.DownArrow)){
-            waitTime = 0.5f;
+        if(Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp("joystick button 1"))
+        {
+            waitTime = 0.1f;
         }
 
-        //The player will be able to jump off the platform if they press the down arrow for a minimum of .5s
-        if(Input.GetKey(KeyCode.DownArrow)){
+        //The player will be able to jump off the platform if they press the down arrow for a minimum of .1s
+        if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S) || Input.GetKey("joystick button 1"))
+        {
             if(waitTime <=0){
                 effector.rotationalOffset = 180f;
-                waitTime = 0.5f;
+                waitTime = 0.1f;
             } else{
                 waitTime -= Time.deltaTime;
             }
         }
 
         //Return the offset to 0 so the player can jump through the platform and stand over it.
-        if(Input.GetKey(KeyCode.Space)){
+        if(Input.GetKey(KeyCode.Space) || Input.GetKey("joystick button 0"))
+        {
             effector.rotationalOffset = 0;
         }
     }
