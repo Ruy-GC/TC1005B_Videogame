cd3935fadec376f9c71e439ebd4e5dc744b920c6
Ruy Guzmán Camacho <ruy.guzman2002@gmail.com>
2022-03-03 23:03:10 -0600

    simple ui interaction on collision with enemy

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class Player : MonoBehaviour
     //enemy collision
     public float force = 5;
     public ForceMode2D forceMode = ForceMode2D.Impulse;
+    public GameObject damageUI;
+    Image damageImg;
 
     //ground check
     public float distanceToCheck = 0.1f;
@@ -21,6 +24,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        damageImg = damageUI.GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -63,15 +67,30 @@ public class Player : MonoBehaviour
     //al chocar con jugador
     void OnCollisionEnter2D(Collision2D c){
         if(c.gameObject.tag == "enemy"){
-            print("pp");
+            //gets vomtact point and player position
             ContactPoint2D contactPoint = c.GetContact(0);
             Vector2 playerPosition = transform.position;
+
+            //sets direction to the oppoite of the collision
             Vector2 dir = contactPoint.point - playerPosition;
             dir = -dir.normalized;
+            
+            //UI flash
+            StartCoroutine(damageFlash());
 
+            //Uses physics to impulse the player
             GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
             GetComponent<Rigidbody2D>().inertia = 0;
             GetComponent<Rigidbody2D>().AddForce(dir * force, forceMode);
         }
     }
+
+    IEnumerator damageFlash(){
+            var alpha= damageImg.color;
+            damageImg.color = new Color32(255,0,0,100);
+            yield return new WaitForSeconds (0.1f);
+            damageImg.color = new Color32(255,0,0,0);
+    }
 }
+
+
